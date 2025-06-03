7bd77ca70b82107ed2965e7694c9a1a7211b7a82
TomRegnier <tomregnier83@gmail.com>
2025-06-03 08:18:16 +0200

    Commit reglage scroll

--- script.js
+++ script.js
@@ -1,24 +1,9 @@
-let startY = 0;
-let pulling = false;
-
 const squaredown = document.querySelector(".viewmoney");
 
 squaredown.addEventListener('touchstart', (e) => {
-    startY = e.touches[0].clientY;
-    pulling = false;
+    squaredown.style.backgroundColor = "red";
 });
 
-squaredown.addEventListener('touchmove', (e) => {
-    const currentY = e.touches[0].clientY;
-    if (currentY - startY > 50) {
-        pulling = true;
-        squaredown.style.backgroundColor="red";
-    }
-})
-
 squaredown.addEventListener('touchend', () => {
-    if(pulling) {
-        console.log("Pulling");
-        squaredown.style.backgroundColor="white";
-    }
-})
+    squaredown.style.backgroundColor = "white";
+});
